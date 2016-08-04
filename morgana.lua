4b14c7990f6fb728e0f1fd6f7df4a34b0eb3e2c4
thekingofasome <anthonybnguyen56@yahoo.com>
2016-08-04 16:58:52 -0600

    Update morgana.lua

--- morgana.lua
+++ morgana.lua
@@ -13,12 +13,12 @@ end
 require('OpenPredict')
 
 --MENU
-local MorgMenu = Menu("Morgana", "Morgana")						-			--Create a Menu and called MorgMenu (Users sees "Morgana")
-MorgMenu:SubMenu("Combo", "Combo")							    			--Create a New SubMenu and call it Combo
-MorgMenu.Combo:Boolean("Q", "Use Q", true)									--Add a button to toggle the usage of Q
-MorgMenu.Combo:Boolean("W", "Use W", true)									--Add a button to toggle the usage of W
-MorgMenu.Combo:Boolean("R", "Use R", true)									--Add a button to toggle the usage of R
-MorgMenu.Combo:Boolean("KSW", "Killsteal with W", true)		    			--Add a button to killsteal with W
+local MorgMenu = Menu("Morgana", "Morgana")					--Create a Menu and called MorgMenu (Users sees "Morgana")
+MorgMenu:SubMenu("Combo", "Combo")						--Create a New SubMenu and call it Combo
+MorgMenu.Combo:Boolean("Q", "Use Q", true)					--Add a button to toggle the usage of Q
+MorgMenu.Combo:Boolean("W", "Use W", true)					--Add a button to toggle the usage of W
+MorgMenu.Combo:Boolean("R", "Use R", true)					--Add a button to toggle the usage of R
+MorgMenu.Combo:Boolean("KSW", "Killsteal with W", true)		    		--Add a button to killsteal with W
 
 local MorgQ = {delay = 0.25, range = 1175, radius = 65, speed = 1200}		--Table for Morg Q
 local MorgW = {delay = 0.25, range = 900, radius = 225, speed = math.huge}	--TABLE for Morg W
