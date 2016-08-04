f84cd5951de827ed6495ebc034cae06bee16d3c4
thekingofasome <anthonybnguyen56@yahoo.com>
2016-08-04 12:20:31 -0600

    Update morgana.lua

--- morgana.lua
+++ morgana.lua
@@ -1,8 +1,6 @@
 --CHAMPION
 myHero.charName ~= "Morgana" then return end --Character
 
-
-
 --MIXLIB
 if FileExist(COMMON_PATH.."MixLib.lua") then --MIXLIB
  require('MixLib')
@@ -23,7 +21,7 @@ MorgMenu.Combo:Boolean("R", "Use R", true)						--Add a button to toggle the usa
 MorgMenu.Combo:Boolean("KSW", "Killsteal with W", true)		    --Add a button to killsteal with W
 
 local MorgQ = {delay = 0.25, range = 1175, radius = 65, speed = 1200}	--Table for Morg Q
-local MorgW = {delay = 0.5, range = 900, radius = 225, speed = 2200}	--TABLE for Morg W
+local MorgW = {delay = 0.5, range = 900, radius = 225, speed = math.huge}	--TABLE for Morg W
 
 OnTick(function(myHero)
 	local target = GetCurrentTarget()
@@ -42,4 +40,10 @@ OnTick(function(myHero)
             end
 
 		if MorgMenu.Combo.W:Value() and Ready(_W) and ValidTarget(target, 900) then
-
+			--[[
+				MorgMenu.Combo.W:Value() returns true if the menu has been ticked
+				Ready(_W) returns true if we are able to cast W now
+				ValidTarget(target, 900) returns true if the target can be attacked and is in a range of 900 (Morg W range)
+			]]
+			local targetPos = GetOrigin(target)		--saves the XYZ coordinates of the target to the variable
+			CastSkillShot(_W , targetPos)			--Since the W is a skillshot (select area), we have to cast it at a point on the ground (targetPos)
