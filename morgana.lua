b8794716df1fa22219e8a1286f733cc9bdaf61ad
thekingofasome <anthonybnguyen56@yahoo.com>
2016-08-04 14:49:53 -0600

    Update morgana.lua

--- morgana.lua
+++ morgana.lua
@@ -35,7 +35,6 @@ OnTick(function(myHero)
 				Ready(_Q) returns true if we are able to cast Q now
 				ValidTarget(target, 1175) returns true if the target can be attacked and is in a range of 1175 (Morg Q range)
 			]]		
-       local QPred = GetLinearAOEPrediction(target, MorgQ, sourcePos)
 		if QPred.hitchance >= 0.6 
                                    CastSkillShot(_Q, predQ.castPos)
 		end 	--Ends the W logic
