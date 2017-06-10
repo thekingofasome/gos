d8c24f7b91f840027e03c46d1b42bc038c29f0f4
thekingofasome <anthonybnguyen56@yahoo.com>
2017-06-10 15:19:50 -0500

    Update morgana.lua

--- morgana.lua
+++ morgana.lua
@@ -1,83 +1,76 @@
---CHAMPION
-if myHero.charName ~= "Morgana" then return end
+--Morgana
+if GetObjectName(GetMyHero()) ~= "Morgana" then return end
 
---MIXLIB
-if FileExist(COMMON_PATH.."MixLib.lua") then --MIXLIB
+if FileExist(COMMON_PATH.."MixLib.lua") then
  require('MixLib')
 else
  PrintChat("MixLib not found. Please wait for download.")
  DownloadFileAsync("https://raw.githubusercontent.com/VTNEETS/NEET-Scripts/master/MixLib.lua", COMMON_PATH.."MixLib.lua", function() PrintChat("Downloaded MixLib. Please 2x F6!") return end)
 end 
 
---OPENPREDICT
 require('OpenPredict')
 
---MENU
-local MorgMenu = Menu("Morgana", "Morgana")					--Create a Menu and called MorgMenu (Users sees "Morgana")
-MorgMenu:SubMenu("Combo", "Combo")						--Create a New SubMenu and call it Combo
-MorgMenu.Combo:Boolean("Q", "Use Q", true)					--Add a button to toggle the usage of Q
-MorgMenu.Combo:Boolean("W", "Use W", true)					--Add a button to toggle the usage of W
-MorgMenu.Combo:Boolean("R", "Use R", true)					--Add a button to toggle the usage of R
-MorgMenu.Combo:Boolean("KSW", "Killsteal with W", true)		    		--Add a button to killsteal with W
+local MorgMenu = Menu("Morgana", "Morgana")
+MorgMenu:SubMenu("Combo", "Combo")		
+MorgMenu.Combo:Boolean("Q", "Use Q", true)				
+MorgMenu.Combo:Boolean("W", "Use W", true)	
+MorgMenu.Combo:Boolean("E", "USE E", true)
+MorgMenu.Combo:Boolean("R", "USE R", true)
 
-local MorgQ = {delay = 0.25, range = 1175, radius = 65, speed = 1200}		--Table for Morg Q
-local MorgW = {delay = 0.25, range = 900, radius = 225, speed = math.huge}	--TABLE for Morg W
+MorgMenu:SubMenu("LC", "Lane Clear")
+MorgMenu.LC:Boolean("W", "Use W", true)
+MorgMenu.LC:Slider("MM", "Minimum mana to cast lane clear", 60, 100, 2)
 
-OnTick(function(myHero)
-	local target = GetCurrentTarget()
+MorgMenu:SubMenu("HC", "Hit chance of spell")
+MorgMenu.HC:Slider("QHC", "Hit chance of Q", 60, 0, 100, 2)
+MorgMenu.HC:Slider("WHC", "Hit chance of W", 60, 0, 100, 2)
 
-	if Mix:Mode() == "Combo" then
+MorgMenu:SubMenu("DW", "Drawings")
+MorgMenu.DW:Boolean("Q", "Draw Q Range", true)
+MorgMenu.DW:Boolean("W", "Draw W Range", true)
+MorgMenu.DW:Boolean("E", "Draw e Range", true)
+MorgMenu.DW:Boolean("R", "Draw R Range", true)
+MorgMenu.DW:Boolean("DWR", "Draw When Skills are ready", true)
 
-		--Q LOGIC
-		if MorgMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 1175) then
-		--[[
-				MorgMenu.Combo.Q:Value() returns true if the menu has been ticked
-				Ready(_Q) returns true if we are able to cast Q now
-				ValidTarget(target, 1175) returns true if the target can be attacked and is in a range of 1175 (Morg Q range)
-			]]		
-		if QPred.hitchance >= 0.6 then
-                                   CastSkillShot(_Q, predQ.castPos)
-		end 	--Ends the Q logic
- 	end
-		--W LOGIC
-		if MorgMenu.Combo.W:Value() and Ready(_W) and ValidTarget(target, 900) then
-			--[[
-				MorgMenu.Combo.W:Value() returns true if the menu has been ticked
-				Ready(_W) returns true if we are able to cast W now
-				ValidTarget(target, 900) returns true if the target can be attacked and is in a range of 900 (Morg W range)
-			]]
-			local targetPos = GetOrigin(target)		--saves the XYZ coordinates of the target to the variable
-			CastSkillShot(_W , targetPos)			--Since the W is a skillshot (select area), we have to cast it at a point on the ground (targetPos)
-		end		--Ends the W logic
+local MorgQ = {delay = 0.25, range = 1175, radius = 65, speed = 1200}
+local MorgW = {delay = 0.25, range = 900, radius = 225, speed = math.huge}	
+
+OnDraw(function()
+	if MorgMenu.DW.ODR:Value() then
+
+		if Ready(_Q) and MorgMenu.DW.Q:Value() then
+			DrawCircle(myHero,GetCastRange(myHero,_Q),1,25,GoS.Blue)
+		end
+
+		if Ready(_W) and MorgMenu.DW.W:Value() then
+			DrawCircle(myHero,GetCastRange(myHero,_W),1,25,GoS.Pink)
+		end
+
+		if Ready(_E) and MorgMenu.DW.E:Value() then
+			DrawCircle(myHero,GetCastRange(myHero,_E),1,25,GoS.Green)
+		end
+
+		if Ready(_R) and MorgMenu.DW.R:Value() then
+			DrawCircle(myHero,GetCastRange(myHero,_R),1,25,GoS.Yellow)
+		end
+
+	else
+
+		if MorgMenu.DW.Q:Value() then
+			DrawCircle(myHero,GetCastRange(myHero,_Q),1,25,GoS.Blue)
+		end
+
+		if MorgMenu.DW.W:Value() then
+			DrawCircle(myHero,GetCastRange(myHero,_W),1,25,GoS.Pink)
+		end
+
+		if MorgMenu.DW.E:Value() then
+			DrawCircle(myHero,GetCastRange(myHero,_E),1,25,GoS.Green)
+		end
+
+		if MorgMenu.DW.R:Value() then
+			DrawCircle(myHero,GetCastRange(myHero,_R),1,25,GoS.Yellow)
+		end
 	end
-		--R LOGIC
-		if MorgMenu.Combo.R:Value() and Ready(_R) and ValidTarget(target, 200) then		--Same check as Q/W
-					CastSpell(_R , targetPos)
-				end
-			end
-		end	--Ends the R logic
-	end		--Ends the Combo Mode
+end)
 
-
-		--ks part
-			for _, enemy in pairs(GetEnemyHeroes()) do 		--This will cycle 5 times and pass one hero to enemy each cycle
-		if MorgMenu.Combo.Q:Value() and MorgMenu.Combo.KSQ:Value() and Ready(_Q) and ValidTarget(enemy, 1175) then
-			--[[
-				Q Dmg Calc
-				MAGIC DAMAGE: 80 / 135 / 190 / 245 / 300
-				Level 0: 45
-				Level 1: 80
-				Level 2: 115 ...
-				Formula 45 + 35 * CastLevel of Q + AP *0.8
-				CalcDamage(startUnit, targetUnit, normalDamage, magicDamage)
-			--]]
-						if GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, 45 + 55 * GetCastLevel(myHero,_Q) + GetBonusAP(myHero) * 0.9) then
-				local QPred = GetLinearAOEPrediction(target, MorgQ, sourcePos)
-				if QPred.hitchance >= 0.6 
-				CastSkillShot(_Q, predQ.castPos)
-			end										--End dmg check for Q
-		end				--end basic check for Q
-	end			--end KS
-end)		--End script
-
-print("Morgana loaded")	--Little message to show that the script has injected without breaking
